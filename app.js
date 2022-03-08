aee3f3df7bfc1799e0654d448bb20dbb1889a924
2019030847 <101176900+2019030847@users.noreply.github.com>
2022-03-08 02:25:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("Iniciando Servidor");
+})
+
+const puerto = 3000;
+app.listen(puerto,()=>{
+    console.log("Escuchando");
+});
